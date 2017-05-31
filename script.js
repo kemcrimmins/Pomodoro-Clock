cee247e9711e386f749fb7c3e765eb20ae411f1f
Kem Crimmins <kem.crimmins@gmail.com>
2017-05-30 20:34:25 -0400

    initial javascript commit

---
+++ script.js
@@ -0,0 +1,15 @@
+var minutes-display = document.querySelector('#minutes-display');
+var seconds-display = document.querySelector('#seconds-display');
+
+function timer(seconds) {
+
+}
+
+function displayTime(seconds) {
+
+}
+
+/*----- Add Event Listeners Button Bindings ------*/
+
+
+/*----- Begin Button-Binding functions ---------*/
