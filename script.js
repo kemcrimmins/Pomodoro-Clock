a4ef5cd3038837e2c6c3fb45d859015f3b5d5512
Kem Crimmins <kem.crimmins@gmail.com>
2017-06-05 17:43:36 -0400

    implement countdown, displayTime

--- script.js
+++ script.js
@@ -1,13 +1,39 @@
-var minutes-display = document.querySelector('#minutes-display');
-var seconds-display = document.querySelector('#seconds-display');
+var minutesDisplay = document.querySelector('#minutes-display');
+var secondsDisplay = document.querySelector('#seconds-display');
+
+function startTimer() {
+	var pomodoroLengthInSeconds = parseInt(minutesDisplay.textContent) * 60;
+	timer(pomodoroLengthInSeconds);
+}
 
 function timer(seconds) {
+	var timeToStop = Date.now() + seconds * 1000;
 
+	var countdown = setInterval(function () {
+		var secondsLeft = Math.round((timeToStop - Date.now()) / 1000);
+
+		if (secondsLeft < 0) {
+			clearInterval(countdown)
+			return;
+		}
+		displayTimeLeft(secondsLeft);
+	}, 1000);
 }
 
-function displayTime(seconds) {
+function displayTimeLeft(secondsLeft) {
+	var minutes = Math.floor( secondsLeft/ 60);
+	var seconds = secondsLeft % 60; 
 
+	if (seconds < 10) {
+		seconds = '0' + seconds;
+	}
+	
+	minutesDisplay.textContent = minutes;
+	secondsDisplay.textContent = seconds;
+	document.title = minutes + ":" + seconds;
 }
+
+startTimer();
 
 /*----- Add Event Listeners Button Bindings ------*/
 
